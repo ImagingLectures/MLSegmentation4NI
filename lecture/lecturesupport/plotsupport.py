ab09e7456c1e7e12ad08eb114252f8e9b5d2ede3
Anders Kaestner <anders.kaestner@psi.ch>
2021-02-03 18:29:18 +0100

    Filling out slides on problematic cases started to work on k nearest neighbors.

--- lecture/lecturesupport/plotsupport.py
+++ lecture/lecturesupport/plotsupport.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 def buildCMap(plots) :
@@ -7,3 +8,19 @@ def buildCMap(plots) :
         cmaplist.append(p0.get_color())
         
     return ListedColormap(cmaplist)
+
+def magnifyRegion(img,roi, figsize, cmap='gray',vmin=0,vmax=0,title='Original') :
+    if vmin==vmax:
+        vmin=img.min()
+        vmax=img.max()
+    fig, ax = plt.subplots(1,2,figsize=figsize)
+    
+    ax[0].imshow(img,cmap=cmap,vmin=vmin, vmax=vmax)
+    ax[0].plot([roi[1],roi[3]],[roi[0],roi[0]],'r')
+    ax[0].plot([roi[3],roi[3]],[roi[0],roi[2]],'r')
+    ax[0].plot([roi[1],roi[3]],[roi[2],roi[2]],'r')
+    ax[0].plot([roi[1],roi[1]],[roi[0],roi[2]],'r')
+    ax[0].set_title(title)
+    subimg=img[roi[0]:roi[2],roi[1]:roi[3]]
+    ax[1].imshow(subimg,cmap=cmap,extent=[roi[0],roi[2],roi[1],roi[3]],vmin=vmin, vmax=vmax)
+    ax[1].set_title('Magnified ROI')
